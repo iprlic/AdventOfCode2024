3ee39c7e22c392b496c9e020e361478e6912615e
Ivan Prlic <iprlic@gmail.com>
2024-12-19 12:05:35 +0100

    Day 19

---
+++ day19/part2.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+file_path = File.expand_path('input.txt', __dir__)
+
+input = File.read(file_path).split("\n\n")
+$cache = {}
+
+def possible_towel(rule, possible_towels)
+    return $cache[rule] if $cache.has_key?(rule)
+    return 1 if rule == ""
+        
+    $cache[rule] = possible_towels.reduce(0) { |sum, towel| sum += (rule.start_with?(towel) ? possible_towel(rule[towel.size..-1], possible_towels) : 0) }
+    
+    return $cache[rule]
+end
+
+towels = input[0].split(", ").sort_by { |towel| towel.size }.reverse
+rules = input[1].split("\n")
+
+puts rules.map { |rule| possible_towel(rule, towels) }.sum
